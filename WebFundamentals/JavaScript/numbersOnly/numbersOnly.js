1adcf058ebe8209e0c8f0cf0accabedeb0b453da
Max Rohrer <mxrohrer@gmail.com>
2016-12-18 10:57:57 -0800

    fixed issue with numbersOnly function returning the incorrect array. starting work on numRemover which is not currently removing negatives and floats

---
+++ WebFundamentals/JavaScript/numbersOnly/numbersOnly.js
@@ -0,0 +1,26 @@
+var startingArr = [0, "apple", -4, 1.343, false, 88];
+function numbersOnly(startingArr){
+  var numOnly = [];
+  for(var i =0; i<startingArr.length; i++){
+    if(typeof startingArr[i] === "number"){
+      numOnly.push(startingArr[i]);
+    }
+  }
+  return numOnly;
+}
+console.log(numbersOnly(startingArr));
+
+function numRemover(startingArr){
+  var idxPlace = 0;
+  for(i = 0; i<startingArr.length; i++){
+    if(typeof startingArr[i] !== "number"){
+      startingArr[idxPlace] = startingArr[i];
+      idxPlace++;
+    }
+  }
+  for(var j = 0; j<startingArr.length - idxPlace; j++){
+    startingArr.pop();
+  }
+  return startingArr;
+}
+console.log(numRemover(startingArr));
