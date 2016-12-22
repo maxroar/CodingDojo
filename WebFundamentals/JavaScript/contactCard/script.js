7a08a90bd0fa7e6aac32433045dd19308ab24466
Max Rohrer <mxrohrer@gmail.com>
2016-12-21 20:16:32 -0800

    fixed the issue with generated content not having event handlers

--- WebFundamentals/JavaScript/contactCard/script.js
+++ WebFundamentals/JavaScript/contactCard/script.js
@@ -1,31 +1,29 @@
 $(document).ready(function(){
-
+  //the function that creates a card when the button is clicked
   $("#butt").click(function newCard(){
+    //variables to hold the values from the input boxes
     var first = $("#fName").val();
     var last = $("#lName").val();
     var info = $("#info").val();
-
+    //an if statement to make sure all the inputs have text in them
     if(first == "" || last == "" || info == ""){
       alert("Hey, guy, fill in those thangs.");
-    }else{
-      //$("#right").append("<h2 class='show'>" + first + " " + last + "</h2><p>Click for more info...</p><p");
-      $("#right").append(`
+    }else{//this else statement is where the actual function is puts have textexecuted if the in
+      //create a variable that holds the html we want to be inserted into the webpage
+      var cardData = $(`
         <div class='card'>
           <h2 class="show">${first} ${last}</h2>
           <p class="show">Click for more info...</p>
           <p class="hide">${info}</p>
         </div>
-        `);
+        `).on('click', function(){//this .on('click') is saying "when you click the button to create the card then you also want to add the following function which will make the content toggle between shown and hidden"
+            $(this).find('.show').toggle(); //if content is visible then hide it
+            $(this).find('.hide').toggle(); //if content is hidden then show it
+        });
+      //now that everything is properly initiated, append the data with the function already initiated to the #right div
+      $("#right").append(cardData);
+      //here is some code to clear the values of the forms
+
     }
-    $('#right').on('click', 'div', function(){
-
-    })
-    console.log(first, last, info);
   });
-
-  $(".card").click(function flipCard(){
-    $(this).find('.show').toggle();
-    $(this).find('.hide').toggle();
-  });
-
 });
