946eb5bf5649dbb5800b04cfe7d3c066006c9103
Max Rohrer <mxrohrer@gmail.com>
2016-12-25 15:53:02 -0800

    completed aykman layout and movement functionality

--- WebFundamentals/pacman/script.js
+++ WebFundamentals/pacman/script.js
@@ -1,31 +1,100 @@
 $(document).ready(function(){
   var level=[
-    [2,2,2,2,2,2,2,2,2,2],
-    [2,1,1,1,1,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,2],
-    [2,2,2,2,2,2,2,2,2,2]
+    [[2,2,2,2,2,2,2,2,2,2],
+    [2,0,2,1,1,1,2,1,1,2],
+    [2,1,2,1,2,1,1,1,1,2],
+    [2,1,1,1,2,1,2,1,1,2],
+    [2,1,2,1,1,1,2,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2]]
   ];
+  var lvlID = 0;
+  var score = 0;
 
-  function displayLevel(level){
-    var output = '';
-    for(var i = 0; i < level.length; i++){
-      output += '\n<div class="row">';
-      for(var j = 0; j<level[i].length; j++){
-        if(level[i][j] == 2){
-          output += `\n\t<div class="brick"></div>`;
-        }else if(level[i][j] == 1){
-          output += `\n\tdiv class="coin"`;
-        }else if(level[i][j] == 0){
-          output += `\n\t<div class="empty"></div>\n`
-        }
-      }
-      output+='</div>';
-      $('#wrapper').append(output);
-    }
-    console.log(output);
-
+  var aykman = {
+    x: 1,
+    y: 1
   }
 
+  function displayAykman(){
+    document.getElementById('aykman').style.top = aykman.y*20+"px";
+    document.getElementById('aykman').style.left = aykman.x*20+"px";
+  }
+
+  function displayLevel(){
+    var output = '';
+    var winner = 0;
+    for(var i = 0; i < level[lvlID].length; i++){
+      output += '<div class="row">';
+      console.log(level[lvlID][i]);
+      for(var j = 0; j<level[lvlID][i].length; j++){
+        console.log(level[lvlID][i][j])
+        if(level[lvlID][i][j] == 2){
+          output += '<div class="brick"></div>';
+        }else if(level[lvlID][i][j] == 1){
+          output += '<div class="coin"></div>';
+          winner ++;
+        }else if(level[lvlID][i][j] == 0){
+          output += '<div class="empty"></div>'
+        }
+        console.log(output);
+      }
+      output+='</div>';
+    }
+    output+='<div id="aykman"></div>'
+    console.log(output);
+    $('#levelMap').html(output);
+    if(winner == 0){
+      if(lvlID<3){
+        lvlID++;
+        alert(`Winner!\nScore: ${score}\nNext level: ${lvlID+1}`);
+        aykman.x=1;
+        aykman.y=1;
+        displayLevel();
+        displayAykman();
+        displayScore();
+      }
+      else{
+        alert(`Winner!\nScore: ${score}\nYou beat every level!`);
+      }
+    }
+  }
+  function displayScore(){
+    $('#scoreCard').html(score);
+  }
+  $(".lvlBtn").on('click', function(){
+    lvlID = $(this).attr('id');
+    displayLevel();
+    displayAykman();
+    displayScore();
+  })
+
+
+  document.onkeydown = function(e){
+    console.log(e.which);
+    if(e.which == 40){
+      aykman.y ++;
+    }
+    else if(e.which == 39){
+      aykman.x ++;
+    }
+    else if(e.which == 38){
+      aykman.y --;
+    }
+    else if(e.which == 37){
+      aykman.x --;
+    }
+    if(level[lvlID][aykman.y][aykman.x] == 1){
+      level[lvlID][aykman.y][aykman.x] = 0;
+      score+=10;
+    }
+    displayLevel();
+    displayAykman();
+    displayScore();
+  };
+
 });
+//down - 40
+// right - 39
+// left -37
+// up -38
+// space - 32
