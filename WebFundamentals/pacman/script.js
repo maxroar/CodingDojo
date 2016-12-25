7ac160f7a114a7ea42034fe80da3c5e37d7f86a9
Max Rohrer <mxrohrer@gmail.com>
2016-12-24 22:19:35 -0800

    began work on aykman

--- WebFundamentals/pacman/script.js
+++ WebFundamentals/pacman/script.js
@@ -1,3 +1,31 @@
-$(document).ready(function{
-  
+$(document).ready(function(){
+  var level=[
+    [2,2,2,2,2,2,2,2,2,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2]
+  ];
+
+  function displayLevel(level){
+    var output = '';
+    for(var i = 0; i < level.length; i++){
+      output += '\n<div class="row">';
+      for(var j = 0; j<level[i].length; j++){
+        if(level[i][j] == 2){
+          output += `\n\t<div class="brick"></div>`;
+        }else if(level[i][j] == 1){
+          output += `\n\tdiv class="coin"`;
+        }else if(level[i][j] == 0){
+          output += `\n\t<div class="empty"></div>\n`
+        }
+      }
+      output+='</div>';
+      $('#wrapper').append(output);
+    }
+    console.log(output);
+
+  }
+
 });
