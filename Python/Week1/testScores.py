731b6bc78c9abfe1d4dfce42be391f6bca3d0316
Max Rohrer <mxrohrer@gmail.com>
2017-01-08 14:42:09 -0800

    completed python grading function

---
+++ Python/Week1/testScores.py
@@ -0,0 +1,16 @@
+def ten_scores():
+    for num in range(0,10):
+        val = input('Please enter the test score: ')
+        if val >= 90:
+            print('Your grade is A.')
+        elif val >= 80:
+            print('Your grade is B.')
+        elif val >= 70:
+            print('Your grade is C.')
+        elif val >= 60:
+            print('Your grade is D.')
+        else:
+            print('Go back to school, fool.')
+    print('Exiting the program... Goodbye.')
+
+ten_scores()
