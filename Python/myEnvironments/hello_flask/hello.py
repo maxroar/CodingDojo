cc9915c1bd79a5705580ef87a7d698d13e5aa315
Max Rohrer <mxrohrer@gmail.com>
2017-01-10 08:47:45 -0800

    fixed the issue in hello flask where templates weren't rendering

--- Python/myEnvironments/hello_flask/hello.py
+++ Python/myEnvironments/hello_flask/hello.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 app.run(debug=True)
