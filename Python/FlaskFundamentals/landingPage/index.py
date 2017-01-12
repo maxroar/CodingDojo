afe0c14e2ca14d2d1732408327a350c6d9ffa86b
Max Rohrer <mxrohrer@gmail.com>
2017-01-11 22:06:21 -0800

    added input validation to form

--- Python/FlaskFundamentals/landingPage/index.py
+++ Python/FlaskFundamentals/landingPage/index.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
-
+app.secret_key = 'key'
 
 
 @app.route('/')
 def display_index():
-    if not 'data' in session:
-        session['data'] = {}
+    if not 'name' in session:
+        session['name'] = ''
+    if not 'location' in session:
+        session['location'] = ''
+    if not 'language' in session:
+        session['language'] = ''
+    if not 'comment' in session:
+        session['comment'] = ''
     return render_template('index.html')
 
 @app.route('/survey', methods=['POST'])
 def form_submit():
-    session['data']['name'] = request.form['name']
-    session['data']['location'] = request.form['location']
-    session['data']['language'] = request.form['language']
-    session['data']['comment'] = request.form['comment']
+    if len(request.form['name']) < 1:
+        flash('Name cannot be empty!')
+        return redirect('/')
+    elif len(request.form['comment']) < 1:
+        flash('Comment cannot be empty!')
+        return redirect('/')
+    else:
+        session['name'] = request.form['name']
+        session['location'] = request.form['location']
+        session['language'] = request.form['language']
+        session['comment'] = request.form['comment']
     return redirect('/result')
 
 @app.route('/result')
