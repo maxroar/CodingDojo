d0db86451e923976ac8b656825dcbfb056ec22ce
Max Rohrer <mxrohrer@gmail.com>
2017-01-11 22:42:12 -0800

    created success page

--- Python/FlaskFundamentals/registrationForm/index.py
+++ Python/FlaskFundamentals/registrationForm/index.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
 app.secret_key = 'key'
 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
 @app.route('/')
 def display_index():
     if not 'email' in session:
@@ -16,5 +19,16 @@ def display_index():
         session['pass2'] = ''
 
     return render_template('index.html')
+
+@app.route('/form_submit')
+def on_submit():
+    session['email'] = request.form['email']
+    session['first'] = request.form['first']
+    session['last'] = request.form['last']
+    session['pass1'] = request.form['pass1']
+    session['pass2'] = request.form['pass2']
+
+    if len(email) < 5 or email != EMAIL_REGEX:
+        flash('Please enter a valid email', 'error')
 
 app.run(debug=True)
