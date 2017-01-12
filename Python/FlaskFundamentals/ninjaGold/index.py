6de978f543b27313cf43a2123f2729b8a5b2d27e
Max Rohrer <mxrohrer@gmail.com>
2017-01-11 20:14:28 -0800

    completed update_money method

--- Python/FlaskFundamentals/ninjaGold/index.py
+++ Python/FlaskFundamentals/ninjaGold/index.py
@@ -18,18 +18,25 @@ def display_index():
 def update_money():
     name_val = form.request['value']
     money = 0
+    phrase = ''
     if name_val == 'farm':
         money = random.randrange(10,21)
-    elif name_val == 'farm':
+    elif name_val == 'cave':
         money = random.randrange(5,11)
-    elif name_val == 'farm':
+    elif name_val == 'house':
         money = random.randrange(2,6)
-    elif name_val == 'farm':
+    elif name_val == 'casino':
         money = random.randrange(-50,31)
 
 
+    session['total_gold'] += money
 
+    if money < 0:
+        session['activities'] += '<p class="red">Oh noooooo you lost <strong>' + abs(money) + '</strong> gold at the casino!</p>'
+    else:
+        session['activities'] += '<p class="green">You earned <strong>' + money + '</strong> gold at the ' + name_val + '!</p>'
 
+    return redirect('/')
 
 
 app.run(debug=True)
