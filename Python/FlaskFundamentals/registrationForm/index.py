94dad24bd78c76079020e877890175a3f40aadff
Max Rohrer <mxrohrer@gmail.com>
2017-01-11 22:27:30 -0800

    completed html form

--- Python/FlaskFundamentals/registrationForm/index.py
+++ Python/FlaskFundamentals/registrationForm/index.py
@@ -4,7 +4,16 @@ app.secret_key = 'key'
 
 @app.route('/')
 def display_index():
-
+    if not 'email' in session:
+        session['email'] = ''
+    if not 'first_name' in session:
+        session['first_name'] = ''
+    if not 'last_name' in session:
+        session['last_name'] = ''
+    if not 'pass1' in session:
+        session['pass1'] = ''
+    if not 'pass2' in session:
+        session['pass2'] = ''
 
     return render_template('index.html')
 
