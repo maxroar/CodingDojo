fe25b4af27367e2c5ed3349d04d23f38253a3b8f
Max Rohrer <mxrohrer@gmail.com>
2017-01-15 17:57:23 -0800

    began work on email validation

---
+++ Python/FlaskMySQL/emailValidation/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect, request, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+app.secret_key = 'key' #change this
+
+mysql = MySQLConnector(app, 'email_validation')
+#all routes go here
+@app.route('/')
+def display_index():
+    return render_template('index.html')
+# an example of running a query
+# print mysql.query_db("SELECT * FROM table")
+app.run(debug=True)
