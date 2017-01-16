dee823954db3977d5824afa924728e8c3519ea01
Max Rohrer <mxrohrer@gmail.com>
2017-01-15 18:59:59 -0800

    added functionality to add emails to db

--- Python/FlaskMySQL/emailValidation/server.py
+++ Python/FlaskMySQL/emailValidation/server.py
@@ -11,16 +11,18 @@ EMAIL_REGEX = re.compile(r'^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9
 #all routes go here
 @app.route('/')
 def display_index():
+    emails = mysql.query_db('SELECT * FROM emails')
     return render_template('index.html', emails=emails)
 @app.route('/add', methods=['post'])
 def create():
     if not EMAIL_REGEX.match(request.form['email']):
-        error = 'Please enter a valid email address.'
-        return redirect('/', error=error)
+        flash('Please enter a valid email address.', 'error')
+        return redirect('/')
     query = 'INSERT INTO emails (email, created_at, updated_at) VALUES (:email, NOW(), NOW())'
     data = {
         'email': request.form['email']
     }
     mysql.query_db(query, data)
-    return redirect('/')
+    flash('The email address you entered: %s is valid!' % request.form['email'], 'error')
+    return redirect('/success')
 app.run(debug=True)
