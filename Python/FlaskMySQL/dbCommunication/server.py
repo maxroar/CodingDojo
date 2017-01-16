41eb6b4e3afc3b74e6925bda81c31f3482def6d3
Max Rohrer <mxrohrer@gmail.com>
2017-01-15 17:27:41 -0800

    added route to update friend entries

--- Python/FlaskMySQL/dbCommunication/server.py
+++ Python/FlaskMySQL/dbCommunication/server.py
@@ -3,12 +3,21 @@ from mysqlconnection import MySQLConnector
 app = Flask(__name__)
 app.secret_key='key'
 mysql = MySQLConnector(app, 'friendsdb')
-#all routes go here
+
+#root route passes the friends table as a variable to index.html
 @app.route('/')
 def display_index():
     friends = mysql.query_db("SELECT * FROM friends;")
     print(friends);
     return render_template('index.html', all_friends=friends)
+#route for individual friends from the friends table
+@app.route('/friends/<friend_id>')
+def show(friend_id):
+    query = 'SELECT * FROM friends WHERE id = :specific_id'
+    data = {'specific_id': friend_id}
+    friends = mysql.query_db(query, data)
+    return render_template('index.html', one_friend=friends[0])
+#a route to add friends to the db
 @app.route('/friends', methods=['post'])
 def create():
     query = 'INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES (:first_name, :last_name, :occupation, NOW(), NOW())'
@@ -19,12 +28,17 @@ def create():
     }
     mysql.query_db(query, data)
     return redirect('/')
-@app.route('/friends/<friend_id>')
-def show(friend_id):
-    query = 'SELECT * FROM friends WHERE id = :specific_id'
-    data = {'specific_id': friend_id}
-    friends = mysql.query_db(query, data)
-    print(friends)
-    return render_template('index.html', one_friend=friends[0])
+#a route to add friends to the db
+@app.route('/update_friend/<friend_id>', methods=['post'])
+def update():
+    query = 'UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation updated_at = NOW()'
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'occupation': request.form['occupation'],
+        'id' : friend_id
+    }
+    mysql.query_db(query, data)
+    return redirect('/')
 
 app.run(debug=True)
