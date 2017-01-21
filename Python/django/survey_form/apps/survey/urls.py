74bed857557dd321fbc7263858c3d441e5fe4965
Max Rohrer <mxrohrer@gmail.com>
2017-01-20 21:31:41 -0800

    completed survey form

--- Python/django/survey_form/apps/survey/urls.py
+++ Python/django/survey_form/apps/survey/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^clear', views.clear),
+    url(r'^success', views.success),
+    url(r'^proccess', views.proccess),
     url(r'^$', views.index)
 ]
--- Python/django/survey_form/apps/survey/views.py
+++ Python/django/survey_form/apps/survey/views.py
@@ -5,4 +5,21 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 def index(request):
     print('views work!')
+    if 'name' in request.session:
+        print('log out please')
+        return redirect('/success')
     return render(request, 'survey/index.html')
+
+def proccess(request):
+    print('proccess works')
+    if not 'name' in request.session:
+        request.session['name'] = request.POST['name']
+        request.session['location'] = request.POST['location']
+        request.session['language'] = request.POST['language']
+        request.session['comment'] = request.POST['comment']
+    return redirect('/success')
+def success(request):
+    return render(request, 'survey/success.html')
+def clear(request):
+    request.session.flush()
+    return redirect('/')
