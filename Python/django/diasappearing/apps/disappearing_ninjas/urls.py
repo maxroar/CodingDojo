4ad238b8590fd4e827311f36643695f647507455
Max Rohrer <mxrohrer@gmail.com>
2017-01-21 20:06:34 -0800

    completed disappearing ninjas

--- Python/django/diasappearing/apps/disappearing_ninjas/urls.py
+++ Python/django/diasappearing/apps/disappearing_ninjas/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^ninjas/(?P<color>\w+)$', views.ninjas),
     url(r'^', views.index)
 ]
--- Python/django/diasappearing/apps/disappearing_ninjas/views.py
+++ Python/django/diasappearing/apps/disappearing_ninjas/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def index(request):
     return render(request, 'disappearing_ninjas/index.html')
+
+def ninjas(request, color):
+    if color == '':
+        context = { 'image':  'turtles'}
+    elif color == 'red':
+        context = { 'image':  'red'}
+    elif color == 'blue':
+        context = { 'image':  'blue' }
+    elif color == 'purple':
+        context = { 'image':  'purple'}
+    elif color == 'orange':
+        context = { 'image':  'orange'}
+    else:
+        context = { 'image':  'megan'}
+    return render(request, 'disappearing_ninjas/ninjas.html', context)
