a77be5ec4d7569c0910a95b5d2c0f7d47f66c154
Max Rohrer <mxrohrer@gmail.com>
2017-01-22 12:03:26 -0800

    completed data validation for register route

--- Python/django/db_test/apps/dbtest/views.py
+++ Python/django/db_test/apps/dbtest/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from models import User, Message, Comment
+from django.contrib import messages
+from .models import User, Message, Comment
 import re
 
 #regex variables to check new users against
@@ -13,6 +14,22 @@ def index(request):
         return redirect('/login')
     return render(request, 'dbtest/index.html')
 def register(request):
+    fname = request.POST['first_name']
+    lname = request.POST['last_name']
+    email = request.POST['email']
+
+    if not REGEX_NAME.match(request.POST['first_name']):
+        messages.add_message(request, messages.ERROR, 'First name must be only letters and at least 2 characters.')
+    if not REGEX_NAME.match(request.POST['last_name']):
+        messages.add_message(request, messages.ERROR, 'Last name must be only letters and at least 2 characters.')
+    if not REGEX_EMAIL.match(request.POST['email']):
+        messages.add_message(request, messages.ERROR, 'Please use a valid email.')
+    if not REGEX_PASS.match(request.POST['pass1']):
+        messages.add_message(request, messages.ERROR, 'Password must be at least 8 characters and contain at least 1 of each: capital letter, lowercase letter, number, special character.')
+    if not request.POST['pass1'] == request.POST['pass2']:
+        messages.add_message(request, messages.ERROR, 'Passwords must match.')
+    if messages:
+        return redirect('/')
     return redirect('/')
 
 def login(request):
