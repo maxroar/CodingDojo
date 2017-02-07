11e1d0e5f0b003306288230fa6a9ebbc10d134a9
Max Rohrer <mxrohrer@gmail.com>
2017-02-07 13:10:41 -0800

    completed object list practice

--- cSharp/oblist/Program.cs
+++ cSharp/oblist/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collection.Generic;
+using System.Collections.Generic;
 
 namespace ConsoleApplication
 {
@@ -8,6 +8,33 @@ namespace ConsoleApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<object> unboxList = new List<object>();
+            // List[] unbox = new List[];
+            unboxList.Add(7);
+            unboxList.Add(28);
+            unboxList.Add(-1);
+            unboxList.Add(true);
+            unboxList.Add("stringy");
+            
+            int total = 0;
+
+            foreach (object item in unboxList){
+                if (item is int){
+                    total = total + (int)item;
+                    System.Console.WriteLine("This is an int");
+                }
+                else if (item is bool){
+                    System.Console.WriteLine("This is a boolean");
+                }
+                else if (item is string){
+                    System.Console.WriteLine("This is a string");
+                }
+                else{
+                    System.Console.WriteLine("WHAT IS THIS?!?!?");
+                }
+                
+            }
+            System.Console.WriteLine(total);
         }
     }
 }
