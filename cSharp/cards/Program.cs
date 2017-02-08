7518c72236910b165405a260b01d12f287cafeb2
Max Rohrer <mxrohrer@gmail.com>
2017-02-08 11:33:21 -0800

    begginning work on adding types to humans

--- cSharp/cards/Program.cs
+++ cSharp/cards/Program.cs
@@ -13,15 +13,26 @@ namespace ConsoleApplication
                 System.Console.WriteLine(card.stringVal);
                 System.Console.WriteLine(card.suit);
             }
-            System.Console.WriteLine(deck.cards);
             Player max = new Player("Max");
             System.Console.WriteLine(max.name);
             deck.shuffle();
+            foreach (Card card in deck.cards){
+                System.Console.WriteLine(card.cardVal);
+                System.Console.WriteLine(card.stringVal);
+                System.Console.WriteLine(card.suit);
+            }
             System.Console.WriteLine(deck.cards);
             Card newCard = max.draw(ref deck);
+            deck.reset();
+            foreach (Card card in deck.cards){
+                System.Console.WriteLine(card.cardVal);
+                System.Console.WriteLine(card.stringVal);
+                System.Console.WriteLine(card.suit);
+            }
             System.Console.WriteLine(newCard.cardVal);
-                System.Console.WriteLine(newCard.stringVal);
-                System.Console.WriteLine(newCard.suit);
+            System.Console.WriteLine(newCard.stringVal);
+            System.Console.WriteLine(newCard.suit);
+            System.Console.WriteLine(max.hand);
         }
         
     }
