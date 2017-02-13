af77d6bea432cac21dd39173cf4407357c3cc176
Max Rohrer <mxrohrer@gmail.com>
2017-02-13 14:07:28 -0800

    completed mvc get post test

--- cSharp/dotnetcore/test/Controllers/HelloController.cs
+++ cSharp/dotnetcore/test/Controllers/HelloController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 namespace YourNamespace.Controllers
@@ -9,7 +10,19 @@ namespace YourNamespace.Controllers
   public IActionResult Index()
   {
    return View("Index");
-   //Both of these returns will render the same view (You only need one!)
   }
+  [HttpPost]
+    [Route("method")]
+    public IActionResult Method(string TextField, int NumberField)
+    {
+        int numinput = NumberField;
+        System.Console.WriteLine(numinput);
+        string textinput = TextField;
+        System.Console.WriteLine(textinput);
+        Dictionary<string, object> viewData = new Dictionary<string, object>();
+        ViewData["texty"] = textinput;
+        ViewData["numby"] = numinput;
+        return View("Method");
+    }
  }
 }
