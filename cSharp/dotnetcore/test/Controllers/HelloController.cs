d2902861fd17e11b5b45df3a346c2908a864bc7f
Max Rohrer <mxrohrer@gmail.com>
2017-02-13 13:28:46 -0800

    completed mvc test app

---
+++ cSharp/dotnetcore/test/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace YourNamespace.Controllers
+{
+ public class HelloController : Controller
+ {
+  [HttpGet]
+  [Route("")]
+  public IActionResult Index()
+  {
+   return View("Index");
+   //Both of these returns will render the same view (You only need one!)
+  }
+ }
+}
